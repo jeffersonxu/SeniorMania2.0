5ce877cb12a9e58beff00c7459e2e61efe5deae5
jeffersonxu <me@jeffersonxu.com>
2020-08-08 01:01:52 -0400

    added point calculation

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Nav from './Nav'
 import app from "./base"
 import Group from './Group'
@@ -8,6 +8,46 @@ import text from '../challengeData.json'
 
 function Challenges(props){
     const team = props.location.state.data[app.auth().currentUser.uid]
+    useEffect(() => {
+        //Calculate Total Points
+
+        //Special Conditions
+        const janDouble = ["Best Selves™", "Tatusiowie", "Harverd Rejects", "Sen19rs in Predicaments"];
+        const snowcoPoints = {
+            "Sen19rs in Predicaments" : 400,
+            "Osean's Eleven" : 800,
+            "DAB ON 'EM" : 400,
+            "nam" : 300,
+            "Harverd Rejects" : 600
+        }
+
+        let total = 0
+        for(const k in team.challenges.group){
+            if(k === 'townSigns')
+                total += team.challenges.group[k][0] * team.challenges.group[k][1]
+            else if(k === 'snowco' && team.challenges.group[k][0])
+                total += snowcoPoints[team.name]
+            else if(team.challenges.group[k][0])
+                total += team.challenges.group[k][1]
+        }
+
+        for(const k in team.challenges.individual)
+            total += team.challenges.individual[k][0] * team.challenges.individual[k][1]
+
+        for(const k in team.challenges.monthly){
+            if(team.challenges.monthly[k]){
+                if(janDouble.includes(team.name) && k === 'january')
+                    total += 1000
+                else
+                    total += 500
+            }
+        }
+
+        // Database rules set to only read so this does not work anymore
+        // app.database().ref('users/' + app.auth().currentUser.uid).update({
+        //     points: total
+        // });
+    }, [])
 
     return(
         <div className="main">
