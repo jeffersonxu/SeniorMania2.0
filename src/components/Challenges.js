89793717e73af5d86ec42f2d4e68d9ea6574e23f
jeffersonxu <me@jeffersonxu.com>
2020-08-07 23:41:19 -0400

    finished group challenges

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import Nav from './Nav'
 import app from "./base"
+import Group from './Group'
 import Individual from './Individual'
 import Monthly from './Monthly'
 import text from '../challengeData.json'
 
 function Challenges(props){
     const team = props.location.state.data[app.auth().currentUser.uid]
-    const challenges = team.challenges
 
     return(
         <div className="main">
             <Nav data={props.location.state.data}/>
             <h2>Challenges</h2>
-            <Individual data={challenges.individual} text={text.individual}/>
-            <Monthly name={team.name} data={challenges.monthly} text={text.monthly} />
+            <Group data={team.challenges.group} text={text.group} />
+            <Individual data={team.challenges.individual} text={text.individual} />
+            <Monthly name={team.name} data={team.challenges.monthly} text={text.monthly} />
         </div>
     )
-
 }
 
 export default Challenges
---
+++ src/components/Group.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Group(props){
+    const list = Object.keys(props.text).map((key) => {
+        const required = props.text[key].required !== undefined ? <span className="required"> {props.text[key].required}</span>: null
+        if(key === "townSigns"){
+            return(
+                <div key={key} className="custom-control custom-checkbox col-md-6">
+                    <input
+                        type="checkbox"
+                        id={key}
+                        className="custom-control-input"
+                        checked={props.data[key][0] > 0}
+                        disabled={true} />
+                    <label className="custom-control-label" htmlFor={key}>{ props.text[key].text }
+                        <span className="red"> ({props.data[key][1]} per sign, 5250 total)</span>
+                        - Number of signs: <b>{props.data[key][0]}</b>
+                        { required }
+                    </label>
+                </div>
+            )
+        }
+        return(
+            <div key={key} className="custom-control custom-checkbox col-md-6">
+                <input
+                    type="checkbox"
+                    id={key}
+                    className="custom-control-input"
+                    checked={props.data[key][0]}
+                    disabled={true} />
+                <label className="custom-control-label" htmlFor={key}>{ props.text[key].text }
+                    <span className="red"> ({props.data[key][1]} points)</span>
+                    { required }
+                </label>
+            </div>
+        )
+    })
+
+    return(
+        <div className="main">
+            <h3>Group Challenges</h3>
+            <div className="row">
+                { list }
+            </div>
+        </div>
+    )
+}
+
+export default Group
